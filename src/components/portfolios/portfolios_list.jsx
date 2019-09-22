7b31610872878dfd78c7dbb9bb96e1c492e3c726
karenhlai <laikarenh@gmail.com>
2019-09-22 03:34:08 -0700

    Add conuter

--- src/components/portfolios/portfolios_list.jsx
+++ src/components/portfolios/portfolios_list.jsx
@@ -1,32 +1,73 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchPortfolios } from "../../actions/portfolio_actions";
-import Transactions from '../manage/transactions';
+import { fetchPortfolios, fetchPortfolio } from "../../actions/portfolio_actions";
 
 class PortfoliosList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      number: 1
+    }
+  }
+
   componentDidMount() {
-    // this.props.fetchPortfolios();
+    this.props.fetchPortfolios();
+    this.getPortfolio(0);
+  }
+
+  incrementNumber = (e) => {
+    e.preventDefault();
+    
+    if (this.state.number < 10) {
+      this.setState({
+        number: this.state.number += 1
+      });
+      this.getPortfolio(this.state.number-1)
+    }
+  }
+
+  decrementNumber = (e) => {
+    e.preventDefault();
+
+    if (this.state.number > 1) {
+      this.setState({
+        number: this.state.number -= 1
+      });
+      this.getPortfolio(this.state.number-1)
+    }
+  }
+
+  getPortfolio = (id) => {
+    switch (this.props.portfolios) {
+      case null:
+        return;
+      case false:
+        return <div>FALSE!</div>;
+      default: 
+        const displayPortfolio = this.props.portfolios[this.state.number-1];
+        return (
+          <div className="portfolio-container">
+            {displayPortfolio.portfolio_id}
+            {displayPortfolio.portfolio_category}
+            {displayPortfolio.portfolio_description}
+          </div>
+        )
+    }
   }
 
   render() {
-    const portfolios = this.props.portfolios.map(portfolio => {
-      return (
-        <Transactions 
-          key={portfolio.id}
-          portfolio_id={portfolio.portfiolio_id}
-          portfolio_category={portfolio.portfolio_category}
-          portfolio={portfolio}
-        />
-      );
-    });
-
-
     return (
       <div>
-        <h1>
-          Rank your own Risk Temperament
-        </h1>
-        { portfolios }
+        <h1>What is your risk level?</h1>
+        <h3>(Select one)</h3>
+
+        <button onClick={this.decrementNumber}> - </button>
+        <p> {this.state.number}</p>
+        <button onClick={this.incrementNumber}> + </button>
+
+        {this.getPortfolio()}
+
       </div>
     )
   }
@@ -44,6 +85,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchPortfolios: () => dispatch(fetchPortfolios()),
+    fetchPortfolio: (id) => dispatch(fetchPortfolio(id)),
   }
 }
 
--- src/reducers/portfolios_reducer.js
+++ src/reducers/portfolios_reducer.js
@@ -5,15 +5,17 @@ import initialState from '../data/porfolios';
 
 export default (state = initialState, action) => {
   Object.freeze(state);
-  let nextState;
+  let nextState = merge({}, state);
   // debugger
   switch (action.type) {
     case FETCH_PORTFOLIOS: 
       nextState = merge({}, state, action.portfolios);
       return nextState;
     case FETCH_PORTFOLIO:
-      nextState = merge({}, state, { [action.portfolio.id]: action.portfolio } );
-      return nextState;
+      let newState = merge({}, state, { [action.portfolio.id]: action.portfolio } );
+      // return nextState;
+      debugger
+      return nextState[action.portfolio.id] = action.portfolio;
     default:
       return state;
   }
